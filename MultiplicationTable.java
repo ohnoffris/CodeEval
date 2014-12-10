ac2997445d2aba8f029e77440732d238c495070f
Carlos Loução <ohnoffris@gmail.com>
2014-12-10 09:08:02 +0000

    Create MultiplicationTable.java
    Print out the grade school multiplication table upto 12*12.Sollution to the CodeEval challenge.

---
+++ MultiplicationTable.java
@@ -0,0 +1,10 @@
+public class MultiplicationTable {
+    public static void main(String[] args){
+        for (int i=1; i < 13; i++){
+            for(int j=1; j < 13; j++) {
+                System.out.print(String.format("%1$4s", i*j));
+            }
+            System.out.println();
+        }
+    }
+}
